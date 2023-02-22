4e3fd9f6c69426ca8434846b401e6203cf7404b7
shivanandhanj <125817066+shivanandhanj@users.noreply.github.com>
2023-02-22 21:35:24 +0530

    a=100

--- 1.c
+++ 1.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 int main(){
     int a,b,c;
-    a=10;
+    a=100;
     b=20;
     c=a+b;
     printf("%d",a);
-    int d=0;
-    d=69;
-    a=20;
-    printf("newly added one");
     return 0;
 }
